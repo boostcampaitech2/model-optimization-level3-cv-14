bf11109199397494c1a13e540ce51847097adde8
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-11-29 18:01:02 +0900

    initial commit

---
+++ src/modules/__init__.py
@@ -0,0 +1,44 @@
+"""PyTorch Module and ModuleGenerator."""
+
+from src.modules.base_generator import GeneratorAbstract, ModuleGenerator
+from src.modules.bottleneck import Bottleneck, BottleneckGenerator
+from src.modules.conv import Conv, ConvGenerator, FixedConvGenerator
+from src.modules.dwconv import DWConv, DWConvGenerator
+from src.modules.flatten import FlattenGenerator
+from src.modules.invertedresidualv2 import (InvertedResidualv2,
+                                            InvertedResidualv2Generator)
+from src.modules.invertedresidualv3 import (InvertedResidualv3,
+                                            InvertedResidualv3Generator)
+from src.modules.linear import Linear, LinearGenerator
+from src.modules.poolings import (AvgPoolGenerator, GlobalAvgPool,
+                                  GlobalAvgPoolGenerator, MaxPoolGenerator)
+from src.modules.mobilenetv3 import MobileNetV3Generator
+from src.modules.mobilenetv3_torch import MobileNetV3SmallGenerator
+from src.modules.mobilenetv2 import ConvBNActivation
+
+
+__all__ = [
+    "ModuleGenerator",
+    "GeneratorAbstract",
+    "Bottleneck",
+    "Conv",
+    "DWConv",
+    "Linear",
+    "GlobalAvgPool",
+    "InvertedResidualv2",
+    "InvertedResidualv3",
+    "BottleneckGenerator",
+    "FixedConvGenerator",
+    "ConvGenerator",
+    "LinearGenerator",
+    "DWConvGenerator",
+    "FlattenGenerator",
+    "MaxPoolGenerator",
+    "AvgPoolGenerator",
+    "GlobalAvgPoolGenerator",
+    "InvertedResidualv2Generator",
+    "InvertedResidualv3Generator",
+    "MobileNetV3Generator",
+    "MobileNetV3SmallGenerator",
+    "ConvBNActivation",
+]
